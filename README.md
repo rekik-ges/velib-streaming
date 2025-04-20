2ed75f6850652d5cca8da24f7f434ccab3e9051f
rekik-ges <zrekik1@myges.fr>
2025-04-20 16:58:20 +0200

    Ajout du squelette de projet (producer, consumer, kafka, storage, dashboard et config)

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # velib-streaming
 Pipeline Spark streaming Vélib / Producer + Consumer 
+
+
+## Installation et Exécution du Producer
+
+1. **Cloner le dépôt**  
+   ```bash
+   git clone git@github.com:MonOrg/velib-streaming.git
+   cd velib-streaming/producer-pyspark
+# Crée un environnement avec Python 3.10
+pip install --upgrade pip
+pip install pyspark requests kafka-python
+
+# Depuis le dossier producer-pyspark
+python src/producer.py
---
+++ producer-pyspark/src/producer.py
@@ -0,0 +1,39 @@
+import time, json, requests
+from pyspark.sql import SparkSession
+
+# On crée un SparkSession nommé "VelibProducer" pour piloter notre Producer
+spark = SparkSession.builder \
+    .appName("VelibProducer") \
+    .getOrCreate()
+
+VELIB_API = (
+  "https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_information.json"
+)
+
+
+# Boucle infinie pour simuler un flux continu
+
+
+while True:
+    try:
+        # Appel à l’API avec un timeout de 10 s
+        resp = requests.get(VELIB_API, timeout=10)
+        resp.raise_for_status()
+        stations = resp.json()["data"]["stations"]
+    except Exception as e:
+        print(f"Erreur API : {e}")
+        time.sleep(30)
+        continue
+
+    #  Transforme le jdon global en mini‑batches (un dictionnaire par station)
+    for st in stations:
+        record = {
+            "station_id": st["station_id"],
+            "bikes":    st.get("num_bikes_available", None),
+            "docks":    st.get("num_docks_available", None),
+            "ts":       st.get("last_reported", None)
+        }
+        # TO DO : plus tard on enverra via Kafka/socket
+        print(json.dumps(record))
+
+    time.sleep(30)
